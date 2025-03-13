0819c72312a8866da34bd3f5b4ac2902a9ab77ef
david13ean <david13ean@gmail.com>
2025-03-13 17:09:32 -0600

    trial of posts

--- src/app/geofence-map/geofence-map.component.ts
+++ src/app/geofence-map/geofence-map.component.ts
@@ -1,6 +1,12 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, Renderer2 } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  Renderer2,
+} from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
+// import { myApiFunction } from '../../../amplify/functions/api-function/resource';
 import { post } from 'aws-amplify/api';
 
 @Component({
@@ -10,18 +16,18 @@ import { post } from 'aws-amplify/api';
   styleUrl: './geofence-map.component.scss',
 })
 export class GeofenceMapComponent implements AfterViewInit {
-
   friendliness: any;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    private renderer2: Renderer2,) {}
+    private renderer2: Renderer2
+  ) {}
 
   ngAfterViewInit() {
-    const url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBSBGs_SX95q3imJl-3hiZ0wZIQetTJ0k8&libraries=drawing&v=weekly";
+    const url =
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyBSBGs_SX95q3imJl-3hiZ0wZIQetTJ0k8&libraries=drawing&v=weekly';
     this.loadScript(url).then(() => this.initMap());
   }
-
 
   private loadScript(url: string) {
     return new Promise((resolve, reject) => {
@@ -32,14 +38,17 @@ export class GeofenceMapComponent implements AfterViewInit {
       script.onload = resolve;
       script.onerror = reject;
       this.renderer2.appendChild(this.document.head, script);
-    })
+    });
   }
 
   private initMap(): any {
-    const map = new google.maps.Map(document.getElementById('geofence-map') as any, {
-      center: { lat: 40, lng: -111 },
-      zoom: 5,
-    });
+    const map = new google.maps.Map(
+      document.getElementById('geofence-map') as any,
+      {
+        center: { lat: 40, lng: -111 },
+        zoom: 5,
+      }
+    );
 
     const drawingManager = new google.maps.drawing.DrawingManager({
       drawingMode: google.maps.drawing.OverlayType.POLYGON,
@@ -50,22 +59,57 @@ export class GeofenceMapComponent implements AfterViewInit {
           google.maps.drawing.OverlayType.POLYGON,
           google.maps.drawing.OverlayType.RECTANGLE,
         ],
-      }
+      },
     });
 
     drawingManager.setMap(map);
   }
 
   async getPolygonFriendliness() {
-      try {
-        const restOperation = post({ 
-          apiName: 'mobileDataCoords ',
-          path: '/' 
-        });
-        const response = await restOperation.response;
-        console.log('GET call succeeded: ', response);
-      } catch (error: any) {
-        console.log('GET call failed: ', JSON.parse(error.response.body));
-      }
+    // console.log(myApiFunction);
+    this.postItem();
+  }
+
+  async postItem() {
+    try {
+      const restOperation = post({
+        apiName: 'mobileDataCoords',
+        path: '/',
+        options: {
+          body: {
+            message: 'Mow the lawn'
+          }
+        }
+      });
+  
+      const { body } = await restOperation.response;
+      const response = await body.json();
+  
+      console.log('POST call succeeded');
+      console.log(response);
+    } catch (error: any) {
+      console.log('POST call failed: ', JSON.parse(error));
+    }
+    
+
+    try {
+      const restOperation = post({
+        apiName: 'myRestApi',
+        path: 'item',
+        options: {
+          body: {
+            message: 'Mow the lawn'
+          }
+        }
+      });
+  
+      const { body } = await restOperation.response;
+      const response = await body.json();
+  
+      console.log('POST call succeeded');
+      console.log(response);
+    } catch (error: any) {
+      console.log('POST call failed: ', JSON.parse(error));
+    }
   }
 }
